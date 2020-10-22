4b7db48a2a2d9ab07057855423c1f841430ac2b0
Tomas Mejia <tomas.mejia.ed@hotmail.com>
2020-10-21 22:16:18 -0300

    upgrade daniel command version to 1.5

--- src/commands/daniel.ts
+++ src/commands/daniel.ts
@@ -1,14 +1,23 @@
 import { Command } from '../app';
+import { MessageAttachment } from 'discord.js';
 
 const daniel: Command = {
   name: 'daniel',
   description: 'Ah daniel.',
   execute(message, args) {
+    const attachmentArr = [
+      new MessageAttachment('https://i.imgur.com/m29lgZ6.png'),
+      new MessageAttachment('https://i.imgur.com/GoSieMj.mp4'),
+      new MessageAttachment('https://i.imgur.com/LGylL8s.png')
+    ];
+
     const mierDaniel = [
       'hahhaHahaha gay de mierda xD',
       'Quería ir a ayudar a kat pero me dijo que no necesitaba ayuda :^[',
       'No mano, ustedes olvidese de mi',
       'Unos guarsons?',
+      'Se acabó el daniel buena onda',
+      '👌',
       'Estoy trabajanding',
       'Mrco ando haciendo un trabajo mega repetitivo pero que paga bien',
       "Can't",
@@ -33,8 +42,13 @@ const daniel: Command = {
       'NO JODA JESUS',
       'Yo ando activo',
       'Siento que estoy perdiendo mucho tiempo jugando',
-      'Voy a ir a buscar a Kat, ya vengo'
+      'Estoy viendo series con Kat',
+      'GRACIAS DAVID, GRACIAS',
+      'Dame 1 hora y le damos',
+      'Voy a ir a buscar a Kat, ya vengo',
+      ...attachmentArr
     ];
+
     const randomNumber = () => Math.floor(Math.random() * mierDaniel.length);
     message.channel.send(mierDaniel[randomNumber()]);
   }
